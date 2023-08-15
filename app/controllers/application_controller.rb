24f7cde64942953bfa6409b07876d8685c6d7d2a
sakki1226 <142205272+sakki1226@users.noreply.github.com>
2023-08-15 09:15:40 +0900

    posts#indexへのルーティング設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'posts#index'
 end
+
